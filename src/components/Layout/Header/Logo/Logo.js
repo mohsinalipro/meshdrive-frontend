7b96718bae27240f7fde23f375e48acbd4d8cb36
mohsinalipro <contactwithmohsin@gmail.com>
2018-10-04 21:19:37 +0500

    minor logo styling

--- src/components/Layout/Header/Logo/Logo.js
+++ src/components/Layout/Header/Logo/Logo.js
@@ -3,7 +3,7 @@ import "./styles.css";
 export default () => {
   return (
     <div>
-      <h4 className="logo text-center p-1 bg-white mt-2">MeshDrive</h4>
+      <h4 className="logo text-center p-1 bg-white mt-2 ">MeshDrive</h4>
     </div>
   );
 };
