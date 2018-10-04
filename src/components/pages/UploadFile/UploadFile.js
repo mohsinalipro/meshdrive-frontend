a8bb5149a243f6974694276d3f97cd8bb2ac43ee
mohsinalipro <contactwithmohsin@gmail.com>
2018-10-04 22:52:26 +0500

    Added Drag'n'Drop box for file selection + improved Upload Page UI

--- src/components/pages/UploadFile/UploadFile.js
+++ src/components/pages/UploadFile/UploadFile.js
@@ -4,16 +4,37 @@ import SideBar from "../../Layout/SideBar/SideBar";
 import uuid from "uuid";
 import uploadFile from "../../../actions/files/uploadFile";
 import { connect } from "react-redux";
-
+import Dropzone from "react-dropzone";
+import "./styles.css";
 class UploadFile extends Component {
   state = {
-    file: "",
+    onDragEnter: false,
+    files: [],
     drive: ""
   };
 
-  // correction made
-  handleFileChange = event => {
-    this.setState({ file: event.target.files[0] });
+  // file drop
+  //   onDrop= acceptedFiles => {
+  //     const req = request.post('/upload');
+  //     acceptedFiles.forEach(file => {
+  //         req.attach(file.name, file);
+  //     });
+  //     req.end(callback);
+  // }
+  onDrop = acceptedFiles => {
+    this.setState({ files: acceptedFiles });
+    acceptedFiles.forEach(file => {
+      console.log(file);
+      //   const reader = new FileReader();
+      //   reader.onload = () => {
+      //     const fileAsBinaryString = reader.result;
+      //     // do whatever you want with the file content
+      //   };
+      //   reader.onabort = () => console.log("file reading was aborted");
+      //   reader.onerror = () => console.log("file reading has failed");
+
+      //   reader.readAsBinaryString(file);
+    });
   };
 
   handleDriveChange = event => {
@@ -25,7 +46,7 @@ class UploadFile extends Component {
     console.log(event.target.value);
 
     const fileWithInfo = {
-      file: this.state.file,
+      files: this.state.files,
       drive: this.state.drive
     };
 
@@ -38,58 +59,62 @@ class UploadFile extends Component {
   render() {
     return (
       <React.Fragment>
-        <div id="page" className="d-flex flex-row w-100">
+        <div id="page" className="d-flex flex-column w-100">
           <h1>Upload File</h1>
-          <div className="flex-grow-1 d-flex flex-row p-4">
-            <Form onSubmit={this.handleSubmit}>
-              <FormGroup>
-                <Label for="file">File</Label>
-                <Input
-                  type="file"
-                  name="file"
-                  id="file"
-                  onChange={this.handleFileChange}
-                />
-                <FormText color="muted">Select a file to upload.</FormText>
+          <div className="flex-grow-1 d-flex flex-column p-4">
+            <Dropzone
+              className={
+                "filedropzone shadow p-5 mb-2 bg-light rounded border-primary" +
+                (this.state.onDragEnter ? " onDragEnter" : "")
+              }
+              onDrop={this.onDrop}
+            >
+              Try dropping some files here, or click to select files to upload.
+            </Dropzone>
+            <ul>
+              {this.state.files.map(f => (
+                <li key={f.name}>
+                  {f.name} - {f.size} bytes
+                </li>
+              ))}
+            </ul>
+            <FormGroup tag="fieldset">
+              <Label for="drive">Select Drive</Label>
+              <FormGroup check>
+                <Label check>
+                  <Input
+                    type="radio"
+                    name="drive"
+                    value="googledrive"
+                    onChange={this.handleDriveChange}
+                  />{" "}
+                  Google Drive
+                </Label>
               </FormGroup>
-              <FormGroup tag="fieldset">
-                <Label for="drive">Select Drive</Label>
-                <FormGroup check>
-                  <Label check>
-                    <Input
-                      type="radio"
-                      name="drive"
-                      value="googledrive"
-                      onChange={this.handleDriveChange}
-                    />{" "}
-                    Google Drive
-                  </Label>
-                </FormGroup>
-                <FormGroup check>
-                  <Label check>
-                    <Input
-                      type="radio"
-                      name="drive"
-                      value="onedrive"
-                      onChange={this.handleDriveChange}
-                    />{" "}
-                    OneDrive
-                  </Label>
-                </FormGroup>
-                <FormGroup check>
-                  <Label check>
-                    <Input
-                      type="radio"
-                      name="drive"
-                      value="dropbox"
-                      onChange={this.handleDriveChange}
-                    />{" "}
-                    Dropbox
-                  </Label>
-                </FormGroup>
+              <FormGroup check>
+                <Label check>
+                  <Input
+                    type="radio"
+                    name="drive"
+                    value="onedrive"
+                    onChange={this.handleDriveChange}
+                  />{" "}
+                  OneDrive
+                </Label>
               </FormGroup>
-              <Button>Submit</Button>
-            </Form>
+              <FormGroup check>
+                <Label check>
+                  <Input
+                    type="radio"
+                    name="drive"
+                    value="dropbox"
+                    onChange={this.handleDriveChange}
+                  />{" "}
+                  Dropbox
+                </Label>
+              </FormGroup>
+            </FormGroup>
+            <Button onClick={this.handleSubmit}>Submit</Button>
           </div>
 
           {/* <SideBar right>Right UploadFile sidebar</SideBar> */}
